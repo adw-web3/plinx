1cc44ab557300cb865f2a74d04a7d74230fe9afd
adw-web3 <adrian@wrona.ch>
2025-08-24 17:56:25 +0200

    Use new starknet Provider configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,24 @@
 "use client";
 
+import { useEffect } from "react";
 import { NiebieskaKarta } from "@/devlink";
 import { Section, Block, Link } from "@/devlink/_Builtin";
 import { Cta } from "@/devlink/Cta"; // Import the Navbar component
 
 export default function Home() {
+  useEffect(() => {
+    async function initProvider() {
+      const { Provider, constants } = await import("starknet");
+      const provider = new Provider({ nodeUrl: constants.NetworkName.SN_MAIN });
+      console.log("Starknet provider", provider);
+    }
+    void initProvider();
+  }, []);
+
   return (
     <div className="flex items-center justify-center min-h-screen">
       <Cta ctaText="Connect Wallet" />
-      <NiebieskaKarta/>
+      <NiebieskaKarta />
     </div>
   );
 }
