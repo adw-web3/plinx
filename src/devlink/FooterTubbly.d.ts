e9e68a4fc8c4e91bbf3a52bd1f033af73014454f
adw-web3 <adrian@wrona.ch>
2025-09-23 13:33:17 +0200

    add props to footer items

--- src/devlink/FooterTubbly.d.ts
+++ src/devlink/FooterTubbly.d.ts
@@ -1,5 +1,14 @@
 import * as React from "react";
+import * as Types from "./types";
 
 declare function FooterTubbly(props: {
   as?: React.ElementType;
+  footerAktualnosciName?: React.ReactNode;
+  footerAktualnosciLink?: Types.Basic.Link;
+  footerRegulaminLInk?: Types.Basic.Link;
+  footerRegulaminName?: React.ReactNode;
+  footerPolitykaPrywatnosciLink?: Types.Basic.Link;
+  footerPolitykaPrywatnosciName?: React.ReactNode;
+  footerSkrzynkiRegulaminLink?: Types.Basic.Link;
+  footerSkrzynkiRegulaminName?: React.ReactNode;
 }): React.JSX.Element;
